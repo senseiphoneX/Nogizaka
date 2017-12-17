9600dec7432cfd4feca7f7318d2168ba9ba5717a
senseiphoneX <senseiphonex@gmail.com>
2017-12-18 00:09:11 +0900

    Initial Commit

--- Example/Nogizaka/ViewController.swift
+++ Example/Nogizaka/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Nogizaka
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        Nogizaka.memberName()
+        Nogizaka.memberRuby()
+        Nogizaka.ruybList()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Example/Pods/Target Support Files/Nogizaka/Nogizaka-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Nogizaka : NSObject
+@end
+@implementation PodsDummy_Nogizaka
+@end
