327843b652f5204066f76ec14c3818c1c48b38c4
senseiphoneX <33571609+senseiphoneX@users.noreply.github.com>
2017-12-27 08:15:23 +0900

    add member

--- Classes/Main/Nogizaka.swift
+++ Classes/Main/Nogizaka.swift
@@ -28,6 +28,7 @@ public class Nogizaka {
     public static var Members = [Member(LastName: "秋元", FirstName: "真夏", LastRuby: "あきもと", FirstRuby: "まなつ"),
                                  Member(LastName: "生田", FirstName: "絵梨花", LastRuby: "いくた", FirstRuby: "えりか"),
                                  Member(LastName: "生駒", FirstName: "里奈", LastRuby: "いこま", FirstRuby: "りな"),
+                                 Member(LastName: "伊藤", FirstName: "かりん", LastRuby: "いとう", FirstRuby: "かりん"),
                                  Member(LastName: "齋藤", FirstName: "飛鳥", LastRuby: "さいとう", FirstRuby: "あすか"),
                                  Member(LastName: "西野", FirstName: "七瀬", LastRuby: "にしの", FirstRuby: "ななせ"),
                                  Member(LastName: "白石", FirstName: "麻衣", LastRuby: "しらいし", FirstRuby: "まい")]
